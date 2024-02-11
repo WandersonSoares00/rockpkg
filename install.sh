4b4e6181f582028c975b43a9338e3967bd3c7bb4
WandersonSoares00 <wandersonsoared831@gmail.com>
2024-02-11 12:08:35 -0300

    feat: fix relative paths

--- install.sh
+++ install.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
-[[ "$UID" -ne "0" ]] && { echo "You do not have permissions, try sudo ./install.sh" >&2; exit 1; }
+[ "$(id -u)" != "0" ] && { echo "You do not have permissions, try sudo ./install.sh" >&2; exit 1; }
 
 dest='/usr/'
 
-if [ echo $1 | -q "DESTDIR=" ]; then
+if echo "$1" | grep -Eq "^DESTDIR="; then
     dest="${1#DESTDIR=}"
     dest="${dest%/}"
 fi
 
-if ! cp rockpkg "$dest/sbin" && mkdir -p "/usr/lib/rockpkg" \
-    && mkdir -p "$dest/share/doc/rockpkg"; then
+if ! (cp rockpkg "$dest/sbin" && mkdir -p "/usr/lib/rockpkg" \
+    && mkdir -p "$dest/share/doc/rockpkg"); then
     echo "error with $dest path" >&2
+    rm "$dest/sbin/rockpkg" "/usr/lib/rockpkg" "$dest/share/doc/rockpkg"
     exit 1
 fi
 
@@ -19,7 +20,7 @@ chmod +x create help info install remove rockpkg
 
 cp create help info install remove "/usr/lib/rockpkg"
 
-cp README LICENSE "$dest/share/doc/rockpkg"
+cp README.md LICENSE "$dest/share/doc/rockpkg"
 
 echo "Installation successful!"
 
